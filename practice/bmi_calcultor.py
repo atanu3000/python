4fc21a01710285b80d8c5af7a776700edb02fa56
Atanu Paul <atanupaul2411@gmail.com>
2022-12-17 22:57:17 +0530

    Add files via upload

---
+++ practice/bmi_calcultor.py
@@ -0,0 +1,14 @@
+weight = int(input("Enter your weight: "))
+height = float(input("Enter your height: "))
+
+bmi = weight / height ** 2
+print("BMI = " + str(bmi))
+
+if(bmi < 18.5):
+    print("Underweight")
+elif(bmi <= 24.9):
+    print("Healthy Weight")
+elif(bmi <= 29.9):
+    print("Overweight")
+else:
+    print("Obesity")
---
+++ practice/rest_of_life.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your number: "))
+
+days = (90 - age) * 365
+weeks = (90 - age) * 52
+months = (90 - age) * 12
+
+print(f"You have {days} days, {weeks} weeks, {months} months left") 
