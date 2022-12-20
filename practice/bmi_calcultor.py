a1bba04ab898bc573383da0b8499d4781d98aeb4
Atanu Paul <atanupaul2411@gmail.com>
2022-12-20 19:20:44 +0530

    change

--- practice/bmi_calcultor.py
+++ practice/bmi_calcultor.py
@@ -1,7 +1,7 @@
 weight = int(input("Enter your weight: "))
 height = float(input("Enter your height: "))
 
-bmi = weight / height ** 2
+bmi = weight / (height ** 2)
 print("BMI = " + str(bmi))
 
 if(bmi < 18.5):
