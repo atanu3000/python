574821dd2f9690a85edc577dca2cfb2de5de757c
Atanu Paul <atanupaul2411@gmail.com>
2023-04-06 23:40:46 +0530

    new project

--- projects/password_protected_pdf/sourcecode.py
+++ projects/password_protected_pdf/sourcecode.py
@@ -12,11 +12,20 @@
 #     f.close()
 
 from PyPDF2 import PdfWriter, PdfReader
-import getpass
+import getpass, os
 pdfwriter=PdfWriter()
-pdf=PdfReader("voter_id.pdf")
-for page_num in range(len(pdf.pages)):
-  pdfwriter.add_page(pdf.pages[page_num])
+# pdf=PdfReader()
+pdf = []
+for items in os.listdir():
+  if items.endswith('.pdf'):
+    pdf.append(PdfReader(items))
+
+# if os.listdir().endswith('.pdf'):
+#   pdf = PdfReader(items in os.listdir())
+for pdfs in pdf:
+    for page_num in range(len(pdfs.pages)):
+        pdfwriter.add_page(pdfs.pages[page_num])
+
 passw=getpass.getpass(prompt='Enter Password: ')
 pdfwriter.encrypt(passw)
 with open('ho.pdf','wb') as f:
